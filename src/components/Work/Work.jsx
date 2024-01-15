6d923beb547924cae5a542d48c1a48a6284413e9
Sadik <anarul6777@gmail.com>
2024-01-15 18:38:24 +0600

    some update 15 jan

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 const Work = () => {
     return (
@@ -23,10 +25,10 @@ const Work = () => {
 
                 <div className="w-3/4">
                     <h3 className="text-emerald-500 text-center md:text-left text-1xl md:text-2xl mb-2 font-extrabold font-serif">Facebook Marketing</h3>
-                    <p className="text-xs md:text-base text-gray-300">With three years of experience, I offer to enhance your business growth. When working with new businesses, I typically manage a Facebook business page for a month, ensuring optimal results. Why the wait? Building a successful business requires a methodical approach, following business rules for sustained success. If you resonate with this philosophy, welcome to my digital marketing services. Let&apos;s work together for your business success.</p>
-                    <p className="text-white mt-3 text-xs md:text-sm">1. FB business page creation and setup</p>
-                    <p className="text-white text-xs md:text-sm">2. FB free marketing</p>
-                    <p className="text-white text-xs md:text-sm">3. FB paid marketing (adverting) </p>
+                    <p className="text-xs md:text-base inline text-gray-300">With 3 years in Facebook marketing, I specialize in creating impactful online presences. My services include crafting engaging FB business pages, optimizing organic reach through strategic content, and executing targeted paid campaigns. My approach blends technical expertise with a client-centric focus, ensuring alignment with your unique business goals. Choose me as a dedicated partner committed to propelling your brand to new heights in the dynamic realm of social media.</p>
+                    <p className="text-white mt-3 text-xs md:text-sm"> <i className="fa-solid fa-arrow-right"></i> Facebook business page creation and setup</p>
+                    <p className="text-white text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> Facebook free marketing</p>
+                    <p className="text-white text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> Facebook paid marketing (adverting) </p>
                     <Link to="/facebookPreWork">
                         <button className="bg-emerald-400 mb-4 font-extrabold text-white py-2 px-6 rounded-full hover:bg-amber-600 focus:outline-none mt-4">
                             Previous work
@@ -44,10 +46,11 @@ const Work = () => {
                 <div className="w-3/4">
                     <h3 className="text-emerald-500 text-center md:text-left text-1xl md:text-2xl mb-2 font-extrabold font-serif">Youtube Marketing</h3>
                     <p className="text-xs md:text-base text-gray-300">Welcome to a world where your YouTube videos not only captivate but dominate. As a Video SEO specialist, I bring a strategic edge to your content, ensuring it ranks high, reaches the right audience, and thrives in the competitive landscape of online video. From optimized titles, descriptions, and tags to leveraging cutting-edge SEO techniques, I engineer a pathway for your videos to be discovered and cherished. Witness increased views, subscriber growth, and elevated engagement—all while decoding the algorithms that drive YouTube&apos;s success. Let&apos;s amplify your video visibility and make your content the star it deserves to be. Partner with me for Video SEO that unlocks your channel&apos;s full potential.</p>
-                    <p className="text-white mt-3 text-xs md:text-sm">1. Professional YouTube channel creation and setup</p>
-                    <p className="text-white text-xs md:text-sm">2. YouTube video  promotion work</p>
-                    <p className="text-white text-xs md:text-sm">3. YouTube video SEO and Optimisation</p>
-                    <p className="text-white text-xs md:text-sm">4. YouTube channel monetization</p>
+                    <p className="text-white mt-3 text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> Professional YouTube channel creation and setup</p>
+                    <p className="text-white text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> YouTube video  promotion work</p>
+                    <p className="text-white text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> YouTube video SEO and Optimisation</p>
+                    <p className="text-white text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> YouTube ads campaign by Google ads account</p>
+                    <p className="text-white text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> YouTube influencer research</p>
                     <Link to="/youtubePreWork">
                         <button className="bg-emerald-400 mb-4 font-extrabold text-white py-2 px-6 rounded-full hover:bg-amber-600 focus:outline-none mt-4">
                             Previous work
@@ -60,10 +63,10 @@ const Work = () => {
                 <div className="w-3/4">
                     <h3 className="text-emerald-500 text-center md:text-left text-1xl md:text-2xl mb-2 font-extrabold font-serif">Instagram Marketing</h3>
                     <p className="text-xs md:text-base text-gray-300">Ignite your brand&apos;s presence on Instagram with expert marketing by me. Through strategic content creation, targeted audience growth, and innovative campaigns, I&apos;ll transform your Instagram into a captivating visual journey. Engage authentically, build a loyal community, and gain insights with data-driven analytics. Let&apos;s go beyond likes and followers—let&apos;s create meaningful connections. Elevate your digital presence effortlessly with me</p>
-                    <p className="text-white mt-3 text-xs md:text-sm">1. Targeted followers </p>
-                    <p className="text-white text-xs md:text-sm">2. Increase engagement</p>
-                    <p className="text-white text-xs md:text-sm">3. 100% manual growth</p>
-                    <p className="text-white text-xs md:text-sm">4. Instagram influencer research</p>
+                    <p className="text-white mt-3 text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> Targeted followers </p>
+                    <p className="text-white text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> Increase engagement</p>
+                    <p className="text-white text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> 100% manual growth</p>
+                    <p className="text-white text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> Instagram influencer research</p>
                     <Link to="">
                         <button className="bg-emerald-400 mb-4 font-extrabold text-white py-2 px-6 rounded-full hover:bg-amber-600 focus:outline-none mt-4">
                             Previous work
@@ -81,10 +84,10 @@ const Work = () => {
                 <div className="w-3/4">
                     <h3 className="text-emerald-500 text-center md:text-left text-1xl md:text-2xl mb-2 font-extrabold font-serif">Twiter Marketing</h3>
                     <p className="text-xs md:text-base text-gray-300">Unlock the full potential of your brand on Twitter with my strategic marketing expertise. As a Twitter Marketing specialist, I leverage the platform&apos;s dynamic features to boost your brand&apos;s visibility and engagement. From crafting compelling tweets to implementing targeted campaigns, I ensure your message resonates with your audience. With a focus on trending topics, hashtags, and audience analytics, I navigate the Twitter landscape to elevate your presence. Let&apos;s harness the power of concise and impactful communication, driving conversation, followers, and ultimately, conversions. Elevate your brand on Twitter with a tailored approach that captivates and converts.</p>
-                    <p className="text-white mt-3 text-xs md:text-sm">1. Targeted followers </p>
-                    <p className="text-white text-xs md:text-sm">2. Increase engagement</p>
-                    <p className="text-white text-xs md:text-sm">3. 100% manual growth</p>
-                    <p className="text-white text-xs md:text-sm">4. Instagram influencer research</p>
+                    <p className="text-white mt-3 text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> Targeted followers </p>
+                    <p className="text-white text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> Increase engagement</p>
+                    <p className="text-white text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> 100% manual growth</p>
+                    <p className="text-white text-xs md:text-sm"><i className="fa-solid fa-arrow-right"></i> Instagram influencer research</p>
                     <Link to="">
                         <button className="bg-emerald-400 mb-4 font-extrabold text-white py-2 px-6 rounded-full hover:bg-amber-600 focus:outline-none mt-4">
                             Previous work
